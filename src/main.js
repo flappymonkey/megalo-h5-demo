8cdb5429ade7c9e3126d1386560b5875612b0a40
shenbin04 <shenbin04@corp.netease.com>
2019-09-11 20:37:20 +0800

    demo初始化

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import App from './App'
+import Vue from 'vue'
+import VHtmlPlugin from '@megalo/vhtml-plugin'
+
+Vue.use(VHtmlPlugin)
+const app = new Vue(App)
+
+app.$mount()
+
+export default {
+  config: {
+    // pages 的首个页面会被编译成首页
+    pages: [
+      'pages/hello/index',
+      'pages/my/index'
+    ],
+    subPackages: [
+      {
+        root: 'package-new',
+        pages: ['pages/hello2/index']
+      }
+    ],
+    tabBar: {
+      color: '#333',
+      selectedColor: '#007d37',
+      list: [
+        {
+          pagePath: 'pages/hello/index',
+          text: 'home',
+          iconPath: 'native/tabbar/home.png',
+          selectedIconPath: 'native/tabbar/home_on.png'
+        },
+        {
+          pagePath: 'pages/my/index',
+          text: 'my',
+          iconPath: 'native/tabbar/mine.png',
+          selectedIconPath: 'native/tabbar/mine_on.png'
+        }
+      ]
+    },
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'megalo project',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
---
+++ src/pages/my/index.js
@@ -0,0 +1,10 @@
+import App from './index.vue'
+import Vue from 'vue'
+
+const app = new Vue(App)
+
+app.$mount('#app')
+
+export default {
+  config: {}
+}
